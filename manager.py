c05142bd470e5eb62fde2a5e8fb70890a49b32c0
leifeng <isleifeng@gmail.com>
2018-05-28 19:42:56 +0800

    配置mysql

--- manager.py
+++ manager.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    # 配置SQLALchemy
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/infomation'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪改变
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 manager = Manager(app)
 
+db = SQLAlchemy(app)
 
-@app.route('/index')
+
+@app.route('/')
 def index():
     return 'hello flask'
 
